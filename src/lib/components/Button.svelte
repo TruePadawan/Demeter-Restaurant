8d2a5d50f0f756473ba7ac85fd02af6eae003e4d
Chisom Hermes Chigoziri <hermeschigoziri@gmail.com>
2024-03-12 17:29:53 +0100

    create Button component

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	export let outlined = false;
+	export let type: 'button' | 'submit' = 'button';
+</script>
+
+<button
+	{type}
+	class="px-8 py-3 text-lg rounded {outlined
+		? 'border border-white/40 hover:bg-white/40'
+		: 'bg-[#f99118] hover:bg-[#f99018ce]'}"
+>
+	<slot />
+</button>
+
+<style>
+	@import url('https://fonts.googleapis.com/css2?family=Montserrat&display=swap');
+
+	button {
+		font-family: 'Montserrat', sans-serif;
+	}
+</style>
