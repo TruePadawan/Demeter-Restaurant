582544f97454ab760a5410c126e414f0a9908e11
Chisom Hermes Chigoziri <hermeschigoziri@gmail.com>
2024-03-13 17:30:06 +0100

    Create MenuButton.svelte

---
+++ src/lib/components/MenuButton.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	export let menuIsOpen: boolean;
+</script>
+
+<button class="flex flex-col items-end gap-y-1.5 md:hidden" on:click>
+	<div class="bun h-0.5 w-6 bg-white"></div>
+	<div class="bun h-0.5 w-6 bg-white {menuIsOpen ? 'w-4' : ''}"></div>
+	<div class="bun h-0.5 w-6 bg-white {menuIsOpen ? 'w-2' : ''}"></div>
+</button>
+
+<style>
+	.bun {
+		transition: width 0.15s;
+	}
+</style>
