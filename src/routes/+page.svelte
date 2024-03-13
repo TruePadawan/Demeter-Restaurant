b0336f37738e9ca5499807d39ebd1ccbcbf8e111
Chisom Hermes Chigoziri <hermeschigoziri@gmail.com>
2024-03-13 17:30:38 +0100

    add hamburger menu

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,9 +2,38 @@
 	import '../app.css';
 	import logo from '$lib/images/logo.png';
 	import Button from '$lib/components/Button.svelte';
-	import { IconMenu2 } from '@tabler/icons-svelte';
+	import MenuButton from '$lib/components/MenuButton.svelte';
+
+	let menuIsOpen = false;
+
+	function openMenu() {
+		menuIsOpen = true;
+	}
+
+	function closeMenu() {
+		menuIsOpen = false;
+	}
 </script>
 
+<div
+	class="{!menuIsOpen
+		? 'hidden'
+		: ''} absolute flex h-screen w-screen flex-col gap-4 bg-[#333333]/80 px-6 pt-6"
+>
+	<div class="self-end">
+		<MenuButton {menuIsOpen} on:click={closeMenu} />
+	</div>
+	<div class="flex flex-col items-center gap-5 text-xl font-light text-white">
+		<a href="#about-us" class="nav-link">About Us</a>
+		<a href="#menu" class="nav-link">Menu</a>
+		<a href="#events" class="nav-link">Events</a>
+		<a href="#news" class="nav-link">News</a>
+		<a href="#contact" class="nav-link">Contact</a>
+		<hr class="w-full" />
+		<a href="#book-a-table" class="nav-link">Book a table</a>
+		<a href="#see-menu" class="nav-link">See Menu</a>
+	</div>
+</div>
 <div class="hero flex flex-col bg-center xl:bg-top">
 	<nav class="flex items-center justify-between px-6 pt-3 lg:px-32 lg:pt-5">
 		<a class="flex items-center gap-x-1 no-underline" href="/">
@@ -18,9 +47,7 @@
 			<a href="#news" class="nav-link">News</a>
 			<a href="#contact" class="nav-link">Contact</a>
 		</div>
-		<button class="md:hidden">
-			<IconMenu2 stroke={2} />
-		</button>
+		<MenuButton {menuIsOpen} on:click={openMenu} />
 	</nav>
 	<div class="hero-main flex grow flex-col justify-end md:justify-center">
 		<div>
