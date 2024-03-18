02a7f33821a030bfe84395c980f06aa760359596
Chisom Hermes Chigoziri <hermeschigoziri@gmail.com>
2024-03-18 17:23:45 +0100

    move hero section into a separate component

---
+++ src/lib/components/sections/Hero.svelte
@@ -0,0 +1,54 @@
+<script>
+	import logo from '$lib/images/logo.png';
+	import Button from '../Button.svelte';
+	import MenuButton from '../MenuButton.svelte';
+
+	export let menuIsOpen = false;
+
+	function openMenu() {
+		menuIsOpen = true;
+	}
+</script>
+
+<div class="hero flex flex-col bg-center xl:bg-top">
+	<nav class="flex items-center justify-between px-6 pt-3 lg:px-32 lg:pt-5">
+		<a class="flex items-center gap-x-1 no-underline" href="/">
+			<img class="logo" src={logo} alt="demeter logo" />
+			<span class="company-name text-4xl font-bold text-white">DEMETER</span>
+		</a>
+		<div class="hidden gap-x-10 text-xl font-light text-white md:flex">
+			<a href="#about-us" class="nav-link">About Us</a>
+			<a href="#menu" class="nav-link">Menu</a>
+			<a href="#events" class="nav-link">Events</a>
+			<a href="#news" class="nav-link">News</a>
+			<a href="#contact" class="nav-link">Contact</a>
+		</div>
+		<MenuButton {menuIsOpen} on:click={openMenu} />
+	</nav>
+	<div class="hero-main flex grow flex-col justify-end px-[3vw] md:justify-center md:px-[10vw]">
+		<div>
+			<p class="text-7xl font-bold">For Fine Dine</p>
+			<p class="text-6xl font-light">Best Place, Best Service</p>
+		</div>
+		<p class="my-8 text-3xl font-light">We will be happy to see you in our restaurant</p>
+		<div class="hidden flex-col gap-x-8 gap-y-2 sm:flex-row md:flex">
+			<Button>Book a table</Button>
+			<Button outlined>See menu</Button>
+		</div>
+	</div>
+</div>
+
+<style>
+    .hero {
+		/** Darken background image */
+		background-image: linear-gradient(rgba(0, 0, 0, 0.25), rgba(0, 0, 0, 0.25)),
+			url('$lib/images/hero-section.jpg');
+		background-size: cover;
+		height: 100vh;
+	}
+
+	.logo {
+		width: auto;
+		height: 28px;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,16 @@
 <script lang="ts">
 	import '../app.css';
-	import logo from '$lib/images/logo.png';
-	import Button from '$lib/components/Button.svelte';
 	import MenuButton from '$lib/components/MenuButton.svelte';
+	import Hero from '$lib/components/sections/Hero.svelte';
 
 	let menuIsOpen = false;
-
-	function openMenu() {
-		menuIsOpen = true;
-	}
 
 	function closeMenu() {
 		menuIsOpen = false;
 	}
 </script>
 
+<!-- MOBILE HAMBURGER MENU -->
 <div
 	class="{!menuIsOpen
 		? 'hidden'
@@ -34,33 +30,7 @@
 		<a href="#see-menu" class="nav-link">See Menu</a>
 	</div>
 </div>
-<div class="hero flex flex-col bg-center xl:bg-top">
-	<nav class="flex items-center justify-between px-6 pt-3 lg:px-32 lg:pt-5">
-		<a class="flex items-center gap-x-1 no-underline" href="/">
-			<img class="logo" src={logo} alt="demeter logo" />
-			<span class="company-name text-4xl font-bold text-white">DEMETER</span>
-		</a>
-		<div class="hidden gap-x-10 text-xl font-light text-white md:flex">
-			<a href="#about-us" class="nav-link">About Us</a>
-			<a href="#menu" class="nav-link">Menu</a>
-			<a href="#events" class="nav-link">Events</a>
-			<a href="#news" class="nav-link">News</a>
-			<a href="#contact" class="nav-link">Contact</a>
-		</div>
-		<MenuButton {menuIsOpen} on:click={openMenu} />
-	</nav>
-	<div class="hero-main flex grow flex-col justify-end md:justify-center">
-		<div>
-			<p class="text-7xl font-bold">For Fine Dine</p>
-			<p class="text-6xl font-light">Best Place, Best Service</p>
-		</div>
-		<p class="my-8 text-3xl font-light">We will be happy to see you in our restaurant</p>
-		<div class="hidden flex-col gap-x-8 gap-y-2 sm:flex-row md:flex">
-			<Button>Book a table</Button>
-			<Button outlined>See menu</Button>
-		</div>
-	</div>
-</div>
+<Hero {menuIsOpen} />
 
 <style>
 	:global(body) {
@@ -68,29 +38,6 @@
 		margin: 0;
 		color: white;
 		height: 100vh;
-	}
-
-	.hero {
-		/** Darken background image */
-		background-image: linear-gradient(rgba(0, 0, 0, 0.25), rgba(0, 0, 0, 0.25)),
-			url('$lib/images/hero-section.jpg');
-		background-size: cover;
-		height: 100vh;
-	}
-
-	.logo {
-		width: auto;
-		height: 28px;
-	}
-
-	.hero-main {
-		padding: 0 3vw;
-	}
-
-	@media screen and (min-width: 640px) {
-		.hero-main {
-			padding: 0 10vw;
-		}
 	}
 
 	/* not sure why there is some odd space on mobile, this is a quick fix for it */
