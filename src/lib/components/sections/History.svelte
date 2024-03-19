e7636ec7785fb7ce7920da8587e75c1f7a46fc97
Chisom Hermes Chigoziri <hermeschigoziri@gmail.com>
2024-03-19 18:41:50 +0100

    typo

--- src/lib/components/sections/History.svelte
+++ src/lib/components/sections/History.svelte
@@ -8,7 +8,7 @@
 >
 	<img src={HistorySectionImg} alt="demeter restaurant" class="max-h-96 w-full object-cover" />
 	<div class="flex flex-col gap-2">
-		<p class="text-3xl font-light text-[#6e6e6a]">Discovery</p>
+		<p class="text-3xl font-light text-[#6e6e6a]">Discover</p>
 		<p class="text-5xl font-bold text-[#333334]">Our History</p>
 		<p class="my-4 text-2xl font-light text-[#6e6e6a]">
 			A peep at some distant orb has power to raise and purify our thoughts like a strain of sacred
