b5ee3f2ebb0301297ac87023d6b8759143943123
Chisom Hermes Chigoziri <hermeschigoziri@gmail.com>
2024-03-20 20:15:10 +0100

    Create MealItem.svelte

---
+++ src/lib/components/MealItem.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	export let name: string;
+	export let extraInfo: string;
+	export let price: number;
+</script>
+
+<li class="flex justify-between text-[#6e6e6a]">
+	<div class="flex flex-col gap-1">
+		<p class="text-xl font-medium">{name}</p>
+		<p class="font-thin">{extraInfo}</p>
+	</div>
+	<p class="text-xl">${price.toFixed(2)}</p>
+</li>
