6e3a9468181d720448246c238976f94821068515
Chisom Hermes Chigoziri <hermeschigoziri@gmail.com>
2024-03-27 20:00:11 +0100

    Update UpcomingEvents.svelte
    minor styling change

--- src/lib/components/sections/UpcomingEvents.svelte
+++ src/lib/components/sections/UpcomingEvents.svelte
@@ -3,10 +3,10 @@
 	import Button from '../Button.svelte';
 </script>
 
-<div id="events" class="flex flex-col gap-9 bg-[#333333] px-[2vw] py-[6vw] lg:px-[6vw] lg:py-[8vw]">
+<div id="events" class="flex flex-col gap-y-9 bg-[#333333] px-[2vw] py-[6vw] lg:px-[6vw] lg:py-[8vw]">
 	<div class="text-center">
 		<p class="text-2xl">Party with us!</p>
-		<p class="mt-2 text-5xl font-semibold">Upcoming Events</p>
+		<p class="mt-2 text-5xl font-bold">Upcoming Events</p>
 	</div>
 	<div class="flex flex-col lg:flex-row">
 		<img src={EventsImg} alt="a group of people eating" class="max-h-96 object-cover lg:flex-1" />
